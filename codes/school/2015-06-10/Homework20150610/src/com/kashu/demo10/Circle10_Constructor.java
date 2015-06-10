b376b46488551d739b3c2a07d277510550204348
KunAn Hsu <annbigbig@gmail.com>
2015-06-11 07:51:27 +0800

    Homework 2015-06-10 updated 3

---
+++ codes/school/2015-06-10/Homework20150610/src/com/kashu/demo10/Circle10_Constructor.java
@@ -0,0 +1,19 @@
+package com.kashu.demo10;
+
+public class Circle10_Constructor {
+
+	public static void main(String[] args) {
+		//宣告變數
+		Circle cr = new Circle(10,20);
+		
+		//收集資料
+
+		//運算
+		
+		//顯示結果
+		System.out.println("圓面積 = " + cr.getArea());
+		System.out.println("圓柱體體積 = " + cr.getVolume());
+		
+	}
+
+}
