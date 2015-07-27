21714b8a2b334d32e6df7115f5ad47c7b9ebd1b2
KunAn Hsu <annbigbig@gmail.com>
2015-07-27 15:00:30 +0800

    janjan updated [21]

--- codes/school/AnnsProject/janjan/src/main/java/com/kashu/controller/login/LoginController.java
+++ codes/school/AnnsProject/janjan/src/main/java/com/kashu/controller/login/LoginController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.RememberMeAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,7 +18,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +31,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kashu.domain.User;
+import com.kashu.domain.validator.UserPasswordConfirmValidator;
 
 @Controller
 @SessionAttributes({"login_err_flag_session","currentURL"})
