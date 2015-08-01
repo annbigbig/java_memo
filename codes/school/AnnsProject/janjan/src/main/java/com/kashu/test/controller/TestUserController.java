6fbd0221c239ab375e405d9868120a4e936b1a31
KunAn Hsu <annbigbig@gmail.com>
2015-08-01 19:36:36 +0800

    janjan updated [34]

--- codes/school/AnnsProject/janjan/src/main/java/com/kashu/test/controller/TestUserController.java
+++ codes/school/AnnsProject/janjan/src/main/java/com/kashu/test/controller/TestUserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kashu.test.domain.User;
 import com.kashu.test.service.UserService;
 
 @Controller
@@ -18,5 +19,13 @@ public class TestUserController {
 	public String listAllUsers(Model model){
 		model.addAttribute("users", userService.getAllUsers());
 		return "test/testuser01";
+	}
+	
+	@RequestMapping("/add")
+	public String addUser(Model model){
+		User user = new User("sakura","sakurapass","sakura@gmail.com");
+		userService.save(user);
+		model.addAttribute("user",user);
+		return "test/adduser01";
 	}
 }
--- codes/school/AnnsProject/janjan/src/main/java/com/kashu/test/domain/User.java
+++ codes/school/AnnsProject/janjan/src/main/java/com/kashu/test/domain/User.java
@@ -14,6 +14,17 @@ public class User implements Serializable {
 	private String username;
 	private String password;
 	private String email;
+	
+	public User(){
+		
+	}
+	
+	public User(String username,String password,String email){
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
